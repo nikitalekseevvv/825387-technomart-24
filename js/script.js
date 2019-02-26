44cf88de77686103887d4903ddebe9ed801c81a8
Никита Алексеев <supersonicrock2@gmail.com>
2019-02-26 22:31:25 +0400

    Разметка и стили завершены

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
       var linkMap = document.querySelector(".modal-contacts-map");
       var popupMap = document.querySelector(".modal-map");
       var closeMap = document.querySelector(".modal-close-map");
+     
       var linkFeedback = document.querySelector(".modal-feedback-button");
       var popupFeedback = document.querySelector(".modal-feedback");
       var closeFeedback = document.querySelector(".modal-close-feedback");  
-      var login = document.querySelector("[name=name-user]");
-      var formFeedback = popupFeedback.querySelector(".feedback");
-      var nameUserFeedback = popupFeedback.querySelector(".feedback-email");
-      var emailFeedback = popupFeedback.querySelector(".feedback-name");  
-      var textFeedback = popupFeedback.querySelector(".feedback-text"); 
+     
+      var formFeedback = document.querySelector(".feedback");
+      var nameUserFeedback = document.querySelector(".feedback-name");
+      var emailFeedback = document.querySelector(".feedback-email");  
+      var textFeedback = document.querySelector(".feedback-text");
+      
       var linkBuy = document.querySelectorAll(".catalog-item-buy");
       var popupBuy = document.querySelector(".modal-buy");
       var closeBuy = document.querySelector(".modal-close-buy");  
@@ -59,7 +61,8 @@
           evt.preventDefault();
           popupFeedback.classList.add("modal-error");
         }
-  });
+      });
+  
 
       for (var i = 0; i < linkBuy.length; i++) {
         linkBuy[i].addEventListener("click", function (evt) {
