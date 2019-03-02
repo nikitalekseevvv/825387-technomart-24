dede3e923df01b06edef5bebbc57d01b61b4a953
Никита Алексеев <supersonicrock2@gmail.com>
2019-03-02 15:21:43 +0400

    остался js для каталога

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
- var linkMap = document.querySelector(".modal-contacts-map");
+      var linkMap = document.querySelector(".modal-contacts-map");
       var popupMap = document.querySelector(".modal-map");
       var closeMap = document.querySelector(".modal-close-map");
       var linkFeedback = document.querySelector(".modal-feedback-button");
       var popupFeedback = document.querySelector(".modal-feedback");
       var closeFeedback = document.querySelector(".modal-close-feedback");  
-     
       var formFeedback = popupFeedback.querySelector(".feedback");
-      var nameUserFeedback = popupFeedback.querySelector(".feedback-email");
-      var emailFeedback = popupFeedback.querySelector(".feedback-name");  
+      var nameUserFeedback = popupFeedback.querySelector(".feedback-name");
+      var emailFeedback = popupFeedback.querySelector(".feedback-email");  
       var textFeedback = popupFeedback.querySelector(".feedback-text"); 
-      var buttonFeedback = popupFeedback.querySelector(".button");  
-      
+      var buttonFeedback = popupFeedback.querySelector(".button");
       var linkBuy = document.querySelectorAll(".catalog-item-buy");
       var popupBuy = document.querySelector(".modal-buy");
-      var closeBuy = document.querySelector(".modal-close-buy");  
+      var closeBuy = document.querySelector(".modal-close-buy");   
       
       linkMap.addEventListener("click", function (evt) {
         evt.preventDefault();
@@ -28,7 +26,7 @@
       linkFeedback.addEventListener("click", function (evt) {
         evt.preventDefault();
         popupFeedback.classList.add("modal-show");
-        login.focus();
+        nameUserFeedback.focus();
       });
 
       closeFeedback.addEventListener("click", function (evt) {
@@ -63,21 +61,19 @@
           buttonFeedback.offsetWidth = buttonFeedback.offsetWidth;
           buttonFeedback.classList.add("modal-error");
         }
-  });
-
-    for (var i = 0; i < linkBuy.length; i++) {
+      });
+      
+      for (var i = 0; i < linkBuy.length; i++) {
         linkBuy[i].addEventListener("click", function (evt) {
           evt.preventDefault();
           popupBuy.classList.add("modal-show");
         });
       }
-
       closeBuy.addEventListener("click", function (evt) {
         evt.preventDefault();
         popupBuy.classList.remove("modal-show");
       });
         
-
       window.addEventListener("keydown", function(evt) {
         if (evt.keyCode === 27) {
           if (popupBuy.classList.contains("modal-show")) {
